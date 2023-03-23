71de5451860e09d2a10a7317fa315cc1df6c8917
mhmt <memobay3434@gmail.com>
2023-03-23 18:07:28 +0300

    tasındı

--- script.js
+++
@@ -1,6 +0,0 @@
-function yazi_düzenle() {
-    document.getElementById('demo').style.fontSize='50px';
-    document.getElementById('demo').style.textAlign='center';
-    document.getElementById('demo').style.textDecoration='underline';
-    document.getElementById('demo').style.color='red';
-}
